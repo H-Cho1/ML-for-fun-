894b87b0837109ac578cb2e570c205414e1db4e2
Cho.H <89449664+H-Cho1@users.noreply.github.com>
2022-02-27 11:36:11 +0900

    Add files via upload

---
+++ district_restaurant_freq.R
@@ -0,0 +1,33 @@
+install.packages("treemap")
+library("readxl")
+library(dplyr)
+library(treemap)
+
+
+ck <- read_excel("~/Downloads/치킨집_가공.xlsx", encoding= "UTF-8")
+head(ck)
+
+addr <- substr(ck$소재지전체주소, 11, 16)
+head(addr)
+
+addr_num <- gsub("[0-9]", "", addr) # 숫자제거 
+addr_trim <- gsub(" ", "", addr_num) #공백제거
+head(addr_trim)
+
+addr_count <- addr_trim %>%
+    table() %>%   #도수분포표 생성
+    data.frame()
+head(addr_count)
+
+treemap(addr_count, index=".", vSize = "Freq",
+        title="Seodaemoon district chicken restaurants frequency")
+arrange(addr_count, desc(Freq)) %>% head() 
+
+
+
+
+
+
+
+
+
